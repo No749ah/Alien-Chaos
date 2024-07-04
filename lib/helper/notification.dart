27320cc132504008459aeb13b9af1b2da16bafc8
b7bourn <noah.bourgnon@swisslog.com>
2024-07-04 10:47:55 +0200

    ask for notify permissions if not granted

--- lib/helper/notification.dart
+++ lib/helper/notification.dart
@@ -33,11 +33,48 @@ class NotificationController {
       content: NotificationContent(
         id: 10,
         channelKey: 'basic_channel',
-        title: 'Spin Avaiable',
+        title: 'Spin Available',
         body:
-            'You have a new spin avaiable, start the Game to earn beautiful earnings!',
+        'You have a new spin available, start the Game to earn beautiful earnings!',
       ),
-      schedule: NotificationCalendar(hour: 10, repeats: true),
+      schedule: NotificationCalendar(hour: 11, minute: 0, second: 0, repeats: true),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    NotificationController().initialize();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification Demo'),
+      ),
+      body: Center(
+        child: Text(
+          'Daily notification scheduled at 11:00 AM',
+          style: TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
