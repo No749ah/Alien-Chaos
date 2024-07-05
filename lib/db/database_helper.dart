5abb833170efd1a6f47a25993320563b07179f3b
Stephan Wettstein <stephan.wettstein@protecdata.ch>
2024-07-05 11:52:18 +0200

    integration tests

--- lib/db/database_helper.dart
+++ lib/db/database_helper.dart
@@ -14,7 +14,7 @@ class DatabaseHelper {
   Future<Database> get database async {
     if (_database != null) return _database!;
 
-    _database = await _initDB('alienChaosDBData1.db');
+    _database = await _initDB('alienChaos.db');
     return _database!;
   }
 
